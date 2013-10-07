2f2f9e85f010f9a799aa792c87951c454bcd932b
pasberth <pasberth@gmail.com>
2013-10-07 18:04:49 +0900

    出力がドチャクソきれいになった

--- js/lifegame.js
+++ js/lifegame.js
@@ -1,25 +1,56 @@
-( ( this["LifeGame"] = {} ) , ( ( this["LifeGame"]["Cell"] = (function (self$2a, neighbours$2dlist, when$2dborn, when$2dlive, when$2ddead) {
-  return ( ( this["self"] = self$2a ) , ( ( this["neighboursList"] = neighbours$2dlist ) , ( ( this["whenBorn"] = when$2dborn ) , ( ( this["whenLive"] = when$2dlive ) , ( ( this["whenDead"] = when$2ddead ) , null ) ) ) ) );
-}) ) , ( ( this["LifeGame"]["LifeGame"] = (function (cell$2dlist) {
-  return ( ( this["cellList"] = cell$2dlist ) , null );
-}) ) , ( this["LifeGame"]["update"] = (function (life$2dgame) {
+this["LifeGame"] = {};
+this["LifeGame"]["Cell"] = (function ($31, $32, $33, $34, $35) {
+  this["self"] = $31;
+  this["neighboursList"] = $32;
+  this["whenBorn"] = $33;
+  this["whenLive"] = $34;
+  this["whenDead"] = $35;
+  return null;
+});
+this["LifeGame"]["LifeGame"] = (function ($31) {
+  this["cellList"] = $31;
+  return null;
+});
+this["LifeGame"]["update"] = (function (life$2dgame) {
   return Array.prototype.map.call(Array.prototype.map.call(life$2dgame["cellList"], (function (cell) {
-    return ( ( (! ( "true" === jQuery.prototype.attr.call(jQuery(cell["self"]), "isLive") )) && ( Array.prototype.reduceRight.call(cell["neighboursList"], (function (count, neighbours) {
-      return ( ( "true" === jQuery.prototype.attr.call(jQuery(neighbours), "isLive") ) ? ( 1 + count ) : count );
-    }), 0) == 3 ) ) ? (function (_) {
-      return cell["whenBorn"](cell["self"]);
-    }) : ( ( ( "true" === jQuery.prototype.attr.call(jQuery(cell["self"]), "isLive") ) && ( ( Array.prototype.reduceRight.call(cell["neighboursList"], (function (count, neighbours) {
-      return ( ( "true" === jQuery.prototype.attr.call(jQuery(neighbours), "isLive") ) ? ( 1 + count ) : count );
-    }), 0) < 2 ) || ( Array.prototype.reduceRight.call(cell["neighboursList"], (function (count, neighbours) {
-      return ( ( "true" === jQuery.prototype.attr.call(jQuery(neighbours), "isLive") ) ? ( 1 + count ) : count );
-    }), 0) > 3 ) ) ) ? (function (_) {
-      return cell["whenDead"](cell["self"]);
-    }) : ( ( "true" === jQuery.prototype.attr.call(jQuery(cell["self"]), "isLive") ) ? (function (_) {
-      return cell["whenLive"](cell["self"]);
-    }) : (function (x) {
-      return x;
-    }) ) ) );
+    if (((function ($32) {
+      return jQuery.prototype.not.call($32, ("true" === jQuery.prototype.attr.call(jQuery(cell["self"]), "isLive")));
+    }) && (Array.prototype.reduceRight.call(cell["neighboursList"], (function ($31, $32) {
+      if (("true" === jQuery.prototype.attr.call(jQuery($32), "isLive"))) {
+        return (1 + $31);
+      } else {
+        return $31;
+      }
+    }), 0) === 3))) {
+      return (function (_) {
+        return cell["whenBorn"](cell["self"]);
+      });
+    } else if ((("true" === jQuery.prototype.attr.call(jQuery(cell["self"]), "isLive")) && ((2 > Array.prototype.reduceRight.call(cell["neighboursList"], (function ($31, $32) {
+      if (("true" === jQuery.prototype.attr.call(jQuery($32), "isLive"))) {
+        return (1 + $31);
+      } else {
+        return $31;
+      }
+    }), 0)) || (Array.prototype.reduceRight.call(cell["neighboursList"], (function ($31, $32) {
+      if (("true" === jQuery.prototype.attr.call(jQuery($32), "isLive"))) {
+        return (1 + $31);
+      } else {
+        return $31;
+      }
+    }), 0) > 3)))) {
+      return (function (_) {
+        return cell["whenDead"](cell["self"]);
+      });
+    } else if (("true" === jQuery.prototype.attr.call(jQuery(cell["self"]), "isLive"))) {
+      return (function (_) {
+        return cell["whenLive"](cell["self"]);
+      });
+    } else {
+      return (function (x) {
+        return x;
+      });
+    }
   })), (function (callback) {
     return callback([]);
   }));
-}) ) ) ) )
+});
